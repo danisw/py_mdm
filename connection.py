d31af50376fcbcbd069f05271a7eb2e3c4988628
Paramita Daniswari <mitadaniswari@gmail.com>
2024-07-04 15:58:41 +0700

    Update connection.py

--- connection.py
+++ connection.py
@@ -7,16 +7,16 @@ class Connection:
       return "================ Started ==============="
 
   def pdt_connection(self):
-      pdt_conn = pyodbc.connect('Driver={SQL Server};''Server=10.199.16.69;''Database=PDT;''Trusted_Connection=no;''UID=sa;''PWD=Akreditasi2019!;')
+      pdt_conn = pyodbc.connect('Driver={SQL Server};''Server=10.199.16.69;''Database=PDT;''Trusted_Connection=no;''UID=sa;''PWD=xxx;')
       cursor_pdt = pdt_conn.cursor()
       return cursor_pdt
 
   def siakad_connection(self):
-      siakad_conn = pyodbc.connect('Driver={SQL Server};''Server=10.199.16.69;''Database=SIAKAD;''Trusted_Connection=no;''UID=sa;''PWD=Akreditasi2019!;')
+      siakad_conn = pyodbc.connect('Driver={SQL Server};''Server=10.199.16.69;''Database=SIAKAD;''Trusted_Connection=no;''UID=sa;''PWD=xxx;')
       cursor_siakad = siakad_conn.cursor()
       return cursor_siakad
 
   def sipmaba_connection(self):
-      sipmaba_conn = pyodbc.connect('Driver={SQL Server};''Server=10.199.16.69;''Database=SIPMABA;''Trusted_Connection=no;''UID=sa;''PWD=Akreditasi2019!;')
+      sipmaba_conn = pyodbc.connect('Driver={SQL Server};''Server=10.199.16.69;''Database=SIPMABA;''Trusted_Connection=no;''UID=sa;''PWD=xxx;')
       cursor_sipmaba = sipmaba_conn.cursor()
       return cursor_sipmaba
